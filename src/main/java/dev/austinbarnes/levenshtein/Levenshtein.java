2c2ee3044943d11386b7f944736195f9e25e2a0c
Austin Barnes <austin@austinbarnes.dev>
2024-08-22 06:42:21 -0700

    initial commit

--- src/main/java/dev/austinbarnes/levenshtein/Levenshtein.java
+++ src/main/java/dev/austinbarnes/levenshtein/Levenshtein.java
@@ -1,4 +1,11 @@
 package dev.austinbarnes.levenshtein;
 
 public class Levenshtein {
+
+    public boolean isSimilar(String input, String pattern) {
+        if (input == null || pattern == null) {
+            return false;
+        }
+        
+    }
 }
